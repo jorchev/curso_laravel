fb7fdf66400307e7004e46d80439b5048b2d1810
Jorge Vega <j.vega@mafasa.org>
2023-09-29 10:56:31 -0700

    10 - Introducción a Eloquent el ORM de Laravel

---
+++ app/Models/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // protected $table = 'nombre_tabla'; // nombre de tabla personalizado
+}
