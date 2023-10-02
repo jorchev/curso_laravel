2529bed9342815716c657a3b4fff0273ac568c08
Jorge Vega <j.vega@mafasa.org>
2023-10-02 08:48:06 -0700

    20 - Qué significa asignación masiva y por que es un problema

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,22 +30,9 @@ class PostController extends Controller
 
     function store(SavePostRequest $request){
 
-        // $validate = $request->validate([
-        //     'title' => ['required','min:5'],
-        //     'body' => ['required']
-        // ]); // devuelve un array con los campos validados
+        Post::create( $request->validated() );
+        return to_route('posts.index')->with('status','Post created!');
 
-        // $post = new Post;
-        // $post->title = $request->input('title');
-        // $post->body = $request->input('body');
-        // $post->save();
-
-        Post::create( $request->validated() ); // recibe un array con los datos
-
-        // session()->flash('status','Post created!');
-
-        // return redirect()->route('posts.index');
-        return to_route('posts.index')->with('status','Post created!');;
     }
 
     function edit(Post $post){
@@ -54,21 +41,7 @@ class PostController extends Controller
 
     function update(SavePostRequest $request, Post $post){ // function update(Request $request, $post){
 
-        // $validate = $request->validate([
-        //     'title' => ['required','min:5'],
-        //     'body' => ['required']
-        // ]);
-
-        // $post = Post::find($post);
-
-        // $post->title = $request->input('title');
-        // $post->body = $request->input('body');
-        // $post->save();
-
         $post->update( $request->validated() );
-
-        // session()->flash('status','Post updated!');
-        // return to_route('posts.show', $post);
         return to_route('posts.show', $post)->with('status','Post updated!');
 
     }
