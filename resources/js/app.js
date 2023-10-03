a28ea08783a48abd2d5741a9aed0e369c723de46
Jorge Vega <j.vega@mafasa.org>
2023-10-03 12:07:46 -0700

    24 - Cómo instalar Tailwind CSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,1 @@
 import './bootstrap';
-
-console.log('Testing application');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
 <x-layouts.app title="Home" meta-description="Home meta description">
-    <h1>Inicio</h1>
+    <h1 class="text-3xl text-blue-500 font-bold underline">
+        Inicio
+    </h1>
 </x-layouts.app>
