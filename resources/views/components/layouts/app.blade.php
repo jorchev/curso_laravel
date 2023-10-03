51e890b1cf51f5aef774447bb10c89065bbf7a70
Jorge Vega <j.vega@mafasa.org>
2023-10-03 11:41:59 -0700

    22 - Cómo agregar Javascript y CSS (SASS)

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aprendible - {{ $title ?? '' }}</title>
     <meta name="description" content="{{ $metaDescription ?? 'Default meta description' }}">
+    @vite(['resources/css/app.scss','resources/js/app.js'])
 </head>
 <body>
     <x-layouts.navigation />
