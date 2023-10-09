751045118b9e8bd901333dd7c7ad42946e74397b
Jorge Vega <j.vega@mafasa.org>
2023-10-09 09:01:17 -0700

    29 - Cómo crear un formulario de registro de usuarios

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,62 @@
+<x-layouts.app title="Register" meta-description="Contact meta description">
+    <h1 class="my-4 text-3xl text-center text-sky-500 dark:text-sky-500 font-bold">Register</h1>
+
+    <form class="max-w-xl px-8 py-4 mx-auto bg-white rounded shadow dark:bg-slate-800" action="{{ route('register') }}" method="POST">
+        @csrf
+
+        <div class="space-y-4">
+            <label class="flex flex-col">
+                <span class="text-slate-600 dark:text-slate-400">Name</span>
+                <input class="rounded-md shadow-sm border-slate-300 dark:bg-slate-900/80 text-slate-600 dark:text-slate-400 focus:ring focus:ring-slate-300 dark:focus:ring-slate-800 focus:ring-opacity-50 dark:focus:border-slate-700 focus:border-slate-300 dark:bg-slate-800 dark:border-slate-900 dark:text-slate-100 dark:placeholder:text-slate-400" autofocus type="text" name="name" value="{{ old('name') }}">
+                @error('name')
+                <div>
+                    <small class="font-bold text-red-500/80">{{ $message }}</small>
+                </div>
+                @enderror
+            </label>
+        </div>
+
+        <div class="space-y-4">
+            <label class="flex flex-col">
+                <span class="text-slate-600 dark:text-slate-400">Email</span>
+                <input class="rounded-md shadow-sm border-slate-300 dark:bg-slate-900/80 text-slate-600 dark:text-slate-400 focus:ring focus:ring-slate-300 dark:focus:ring-slate-800 focus:ring-opacity-50 dark:focus:border-slate-700 focus:border-slate-300 dark:bg-slate-800 dark:border-slate-900 dark:text-slate-100 dark:placeholder:text-slate-400" type="email" name="email" value="{{ old('email') }}">
+                @error('email')
+                <div>
+                    <small class="font-bold text-red-500/80">{{ $message }}</small>
+                </div>
+                @enderror
+            </label>
+        </div>
+
+        <div class="space-y-4">
+            <label class="flex flex-col">
+                <span class="text-slate-600 dark:text-slate-400">Password</span>
+                <input class="rounded-md shadow-sm border-slate-300 dark:bg-slate-900/80 text-slate-600 dark:text-slate-400 focus:ring focus:ring-slate-300 dark:focus:ring-slate-800 focus:ring-opacity-50 dark:focus:border-slate-700 focus:border-slate-300 dark:bg-slate-800 dark:border-slate-900 dark:text-slate-100 dark:placeholder:text-slate-400" type="password" name="password">
+                @error('password')
+                <div>
+                    <small class="font-bold text-red-500/80">{{ $message }}</small>
+                </div>
+                @enderror
+            </label>
+        </div>
+
+        <div class="space-y-4">
+            <label class="flex flex-col">
+                <span class="text-slate-600 dark:text-slate-400">Password Confirmation</span>
+                <input class="rounded-md shadow-sm border-slate-300 dark:bg-slate-900/80 text-slate-600 dark:text-slate-400 focus:ring focus:ring-slate-300 dark:focus:ring-slate-800 focus:ring-opacity-50 dark:focus:border-slate-700 focus:border-slate-300 dark:bg-slate-800 dark:border-slate-900 dark:text-slate-100 dark:placeholder:text-slate-400" type="password" name="password_confirmation">
+                @error('password_confirmation')
+                <div>
+                    <small class="font-bold text-red-500/80">{{ $message }}</small>
+                </div>
+                @enderror
+            </label>
+        </div>
+
+        <div class="flex items-center justify-between mt-4">
+            <a class="text-sm font-semibold underline border-2 border-transparent rounded dark:text-slate-300 text-slate-600 focus:border-slate-500 focus:outline-none" href="{{ route('login') }}">Login</a>
+            <button class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-center text-white uppercase transition duration-150 ease-in-out border border-2 border-transparent rounded-md dark:text-sky-200 bg-sky-800 hover:bg-sky-700 active:bg-sky-700 focus:outline-none focus:border-sky-500" type="submit">Register</button>
+        </div>
+    </form>
+
+</x-layouts.app>
+
