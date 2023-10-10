a4aca3f2abc8c95c1c55a25169318bb8d940e593
Jorge Vega <j.vega@mafasa.org>
2023-10-10 12:10:15 -0700

    30 - Cómo crear formulario de login y logout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,9 @@
     <h1 class="my-4 text-3xl text-center text-sky-500 dark:text-sky-500 font-bold">
         Inicio
     </h1>
+    @auth
+        <div class="text-white">
+            Authenticated User: {{  Auth::user()->name }}
+        </div>
+    @endauth
 </x-layouts.app>
